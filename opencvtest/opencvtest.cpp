6c3e06be679169fe2befe7086136d79d51b3a8a6
potatozhao <393926004@qq.com>
2013-03-24 21:56:18 +0800

    第一次提交代码。做个纪念。

---
+++ opencvtest/opencvtest.cpp
@@ -0,0 +1,14 @@
+// opencvtest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include"test.h"
+int main()
+{
+	//main2();
+	ImageMain("111.bmp","1.bmp","1.bmp");
+	//test("1.bmp","1.bmp");
+	//main3();
+	//test1234();
+	return 0;
+}
